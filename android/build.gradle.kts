4b8717da539b20120756855474f97395fd5da1b1
ShivrajBabar14 <shivraj.b@coinage.in>
2025-08-11 13:51:35 +0530

    changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")  // Ensure this is the latest version compatible with your Gradle
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.gms:google-services:4.4.2")
         // Add Google Services if using Firebase
         // classpath("com.google.gms:google-services:4.4.0")
     }
